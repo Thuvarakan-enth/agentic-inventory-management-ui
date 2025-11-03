a7a05526f7a477ab26af0130228546bcdd478d03
Binath Perera <binathpr@gmail.com>
2025-11-03 23:56:49 +0530

    get api base url directly.

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ import type {
   User 
 } from '../types';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "http://3.110.122.19:8080/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
