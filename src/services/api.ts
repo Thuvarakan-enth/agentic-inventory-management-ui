4cc8b83faf277d8ee76e12b57c83bbc78a0bf384
Binath Perera <binathpr@gmail.com>
2025-11-05 15:19:58 +0530

    Change api IP address and port number

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ import type {
   User 
 } from '../types';
 
-const API_BASE_URL = "http://3.110.122.19:8080/api";
+const API_BASE_URL = "http://65.2.31.143/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
